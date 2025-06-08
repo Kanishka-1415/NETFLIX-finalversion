91ca273f8a32333632337ea5699dba0032729cd8
Kanishka-1415 <kanishka.00814@gmail.com>
2025-06-08 11:16:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//navbar css prop in scroll
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+//profile redirect
+const profileIcon = document.querySelector(".profile-icon");
+profileIcon.addEventListener("click", () => {
+  window.location.href = "profile-details.html";
+});
+//movie card
+const movieCard = document.querySelectorAll(".content-card");
+movieCard.foreach((card) => {
+  card.addEventListener("mouseEnter", () => {
+    card.style.transform = "scale(1.05)";
+  });
+
+  card.addEventListener("mouseLeave", () => {
+    card.style.transform = "scale(1)";
+  });
+});
